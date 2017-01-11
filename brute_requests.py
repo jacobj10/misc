763fde0959ea6e794e078ffb626d16ace67eb29e
jacobj10 <jacobj10@bbhcsd.org>
2017-01-10 20:24:33 -0500

    Repurposed brute req

--- brute_requests.py
+++ brute_requests.py
@@ -1,23 +1,48 @@
 import requests
 import argparse
+import sleek_logger
+import re
 
 class RequestMachine(object):
     def __init__(self, *args, **kwargs):
         args = self.parse_args()
-        self.wordlist = args.wordlist
         self.url = args.url
+        self.logged = []
+        self.logger = sleek_logger.SleekLogger('SiteTrav.log')
 
     def parse_args(self):
         parser = argparse.ArgumentParser(description='Args for brute forcing requests.')
         parser.add_argument('url', help='an integer for the accumulator')
-        parser.add_argument('--wordlist', default='wordlist.txt', help='alternative wordlist')
         args = parser.parse_args()
         return args
 
     def test_connection(self):
-        while requests.get(self.url).status_code != 200:
-            
+        counter = 0
+        return requests.get(self.url).status_code == 200
 
+
+    def scrape_urls(self, current):
+        try:
+            resp = requests.get(current).text
+        except requests.exceptions.ConnectionError:
+            self.logger.log('Error handling ' + current, 'error')
+        else:
+            matches = re.findall('href=[\'"]?([^\'" >]+)', resp)
+            for match in matches:
+                if '.com' in match or '.edu' in match or '.org' in match:
+                    self.logger.log("{0} is a full url, not traversing".format(match), 'debug')
+                    matches.remove(match)
+                elif match=='/' or match=='\\':
+                    continue
+                else:
+                    new_url = self.url + match
+                    if new_url in self.logged:
+                        continue
+                    else:
+                        self.logged.append(new_url)
+                        self.logger.log('Traversing: ' + new_url)
+                        self.scrape_urls(new_url)
 if __name__ == '__main__':
     x = RequestMachine()
     x.test_connection()
+    x.scrape_urls(x.url)
--- sleek_logger.py
+++ sleek_logger.py
@@ -1,7 +1,16 @@
 from termcolor import colored
 
+import os
+
 class SleekLogger(object):
-	def __init__(self, *args, **kwargs):
+	def __init__(self, name, save_conf=False, rfh=False, rfh_lines=10):
+		self.name = name
+		self.rfh = rfh
+		self.rfh_lines = rfh_lines
+		self.write_mode = 0
+		if self.rfh:
+			self.rfh_WRITE = open(name, 'a')
+			self.rfh_READ = open(name, 'r') # I ain't using rotatingfilehandler
 		self.method_to_type = {
 			'info': '[INFO] ', 'debug': '[DEBUG] ',
 			'warning': '[WARNING] ', 'error': '[ERROR] ' 
@@ -10,17 +19,37 @@ class SleekLogger(object):
 			'info': 'white', 'debug': 'cyan',
 			'warning': 'yellow', 'error': 'red'
 			}
+		self.valid_colors = ['grey', 'red', 'green', 'yellow', 'blue',
+								'magenta', 'cyan', 'white']
 
-	def format_log_string(self, message, method='info'):
+	def add_log_method(self, method, color='white'):
+			if color not in self.valid_colors:
+				self.log("Color: {0} not valid".format(str(color)), 'error')
+
+	def log(self, message, method='info'):
 		if method not in self.method_to_type:
-			self.format_log_string("Log Method: {0} not defined".format(str(method)), 'error')
+			self.log("Log Method: {0} not defined".format(str(method)), 'error')
 			return
 		ret_string = self.method_to_type[method] + message
 		ret_color = self.method_to_color[method]
+		if self.rfh:
+			lines = [line for line in self.rfh_READ]
+			num_lines = 1 + sum(1 for line in lines)
+			if num_lines >= self.rfh_lines:
+				if self.write_mode == 0:
+					self.change_write_mode()
+					self.write_mode = 1
+				to_write = ''.join(lines[1:] + [ret_string,])
+			else:
+				to_write = ret_string + '\n'
+			self.rfh_WRITE.write(to_write)
+			print(num_lines)
 		print(colored(ret_string, ret_color))
 
-SleekLogger().format_log_string('hello')
-SleekLogger().format_log_string('oops', 'warning')
-SleekLogger().format_log_string('oh no', 'error')
-SleekLogger().format_log_string('writing', 'debug')
-SleekLogger().format_log_string('lolcopter', 'poop')
+	def change_write_mode(self):
+		self.rfh_WRITE.close()
+		self.rfh_WRITE = open(self.name, 'w')
+
+	def cleanup(self):
+		self.rfh_READ.close()
+		self.rfh_WRITE.close()
