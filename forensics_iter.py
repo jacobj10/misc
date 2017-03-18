c1be984e9b422ed329d3d96d9c1162a9a0e7a28c
jacobj10 <jacobj10@bbhcsd.org>
2017-03-18 19:34:36 -0400

    Added pixel-diff

--- forensics_iter.py
+++ forensics_iter.py
@@ -19,7 +19,32 @@ def iter_image(path_to_image, cb_func=def_func):
             pixels_with_cb_func.append(cb_func(pix[j,i]))
     return pixels, pixels_with_cb_func
 
-def test_func(pix_struc):
-    print chr(pix_struc[0])
+def diff_image(path1, path2):
+    im1 = Image.open(path1)
+    im2 = Image.open(path2)
+    pix1 = im1.load()
+    pix2 = im2.load()
+    pixel_diff = []
+    width1 = im1.size[0]
+    width2 = im2.size[0]
+    height1 = im1.size[1]
+    height2 = im2.size[1]
+    width = width1
+    height = height1
+    if width1 != width2:
+        print "Widths not equal, using smaller"
+        width = min(width1, width2)
+    if height1 != height2:
+        print "Heights not equal, using smaller"
+        height = min(height1, height2)
+    for i in range(0, height):
+        for j in range(0, width):
+            diffr = pix1[j,i][0] - pix2[j,i][0]
+            diffg = pix1[j,i][1] - pix2[j,i][1]
+            diffb = pix1[j,i][2] - pix2[j,i][2]
+            pixel_diff.append((diffr, diffg, diffb))
+    im3 = Image.new(im1.mode, im1.size)
+    im3.putdata(pixel_diff)
+    im3.save('diff.png')
 
-iter_image('foo.jpg', test_func)
+    return pixel_diff
