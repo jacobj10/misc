24eac620018a6e94ec3f381f8a311ca654d33820
jacobj10 <jacobj10@bbhcsd.org>
2017-04-05 18:58:37 -0400

    Fixed egcd

--- toychest/nc.py
+++ toychest/nc.py
@@ -5,17 +5,9 @@ s = socket.socket(
     socket.AF_INET, socket.SOCK_STREAM)
 #now connect to the web server on port 80
 # - the normal http port
-s.connect(("shell2017.picoctf.com", 44840))
+s.connect(("shell2017.picoctf.com", 7908))
 while True:
 	x = s.recv(1024)
-	print x
-	line =  x.split('\n')[1]
-	parse = line.split("'")
-	first = parse[1]
-	second = parse[3]
-	third = parse[5]
-	send_string = ""
-	for i in range(int(second)):
-		send_string += first
-	send_string += third
-	s.send(send_string + '\n')
+	if "flagperson" in x:
+		print x
+
